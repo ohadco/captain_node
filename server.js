af336b5a2dddccd7406e96a6da588babc76ad4be
Ohad Cohen <ohad66@gmail.com>
2016-09-07 13:42:22 +0300

    Add body parser to server + configure item routes file location

--- server.js
+++ server.js
@@ -7,9 +7,19 @@ var port = 3000;
 // configure the express application variable
 var app = express();
 
+// bodyParser is used to get post request's body
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 // Serving static files from 'app' folder
+// for example, serving static js files as items.js
 app.use(express.static('app'));
+
+// import item routes from itemRoutes.js
+var routes = require('./BE/itemRoutes.js');
+app.use('/', routes);
 
 // start listening to requests on the specified port
 app.listen(port);
-console.log('Server is running on port: ' + port)
+console.log('Server is running on port: ' + port);
